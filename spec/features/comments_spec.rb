c85939282d8538a0f241ab29bedcd4c33123dbc2
tophat8855 <hrfarley@gmail.com>
2014-12-22 15:36:46 -0800

    comment specs

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -10,9 +10,18 @@ feature 'CRUDing comments' do
     click_on "Posting"
     click_on "Add Comment"
     fill_in "Comment", with: "Rad Stuff, Yo."
-    click_on "Submit Comment"
+    click_on "Create Comment"
 
     expect(page).to have_content("Posting")
     expect(page).to have_content("Rad Stuff, Yo.")
+  end
+
+  scenario "User views a comment" do
+    Post.create!(
+    subject: "Posting",
+    post_text: "Some stuff",
+    )
+    visit posts_path
+    click_on "Posting"
   end
 end
