3426ebbae9a328890b41600ccdf43cb3348a6d1d
tophat8855 <hrfarley@gmail.com>
2014-12-22 13:28:48 -0800

    test for creating post

---
+++ spec/features/posts_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'CRUDing posts' do
+  scenario "User creates a post" do
+    visit posts_path
+    click_on "New Post"
+    fill_in "Subject", with: "Weather"
+    fill_in "Post", with: "Can you believe it's snowing?"
+    click_on "Create Post"
+
+    expect(page).to have_content("Weather")
+    expect(page).to have_content("Can you believe it's snowing?")
+  end
+end
