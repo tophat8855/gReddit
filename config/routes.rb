a700cf69a261d663e0d7fa56d56e2870b066db64
tophat8855 <hrfarley@gmail.com>
2014-12-22 15:37:08 -0800

    make things look prettier

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'welcome#index'
+root 'posts#index'
 get '/sign-up' => 'registrations#new', as: :signup
 post '/sign-up' => 'registrations#create'
 get '/sign-in' => 'authentication#new', as: :signin
