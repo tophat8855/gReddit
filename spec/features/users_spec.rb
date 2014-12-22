2283cb8ff8b144d6c5abab49b19b289cc6de871e
tophat8855 <hrfarley@gmail.com>
2014-12-22 11:05:20 -0800

    users test written

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 feature "Users" do
+  scenario "User creates a user" do
+    visit users_path
+    click_on "New User"
+    fill_in "User Name", with: "User1"
+    fill_in "Email", with: "fake@email.com"
+    fill_in "Password", with: "123"
+    fill_in "Password confirmation", with: "123"
+    click_on "Register"
 
+    expect(page).to have_content ("Welcome, #{User1}")
+  end
 end
