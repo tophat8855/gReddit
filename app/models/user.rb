d4205c3a9c979fd9bb6f41fae0ea6df8bffff533
tophat8855 <hrfarley@gmail.com>
2014-12-22 15:31:45 -0800

    associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :comments
+  has_many :posts
+
   validates :email, presence: true, uniqueness: true, confirmation: true
   validates :user_name, presence: true, uniqueness: true
   has_secure_password
