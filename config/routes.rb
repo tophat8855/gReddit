582fde2ec01f7fb2fd82f09d44411b62e3f0e7d2
tophat8855 <hrfarley@gmail.com>
2014-12-22 11:58:31 -0800

    navbar set up

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 root 'welcome#index'
 get '/sign-up' => 'registrations#new', as: :signup
 post '/sign-up' => 'registrations#create'
-
+get '/sign-in' => 'authentication#new', as: :signin
+post '/sign-in' => 'authentication#create'
+get '/sign-out' => 'authentication#destroy', as: :signout
 resources :users
 
   # Example of regular route:
