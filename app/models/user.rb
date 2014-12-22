207a1b28d878989b8a5f00b7d489d20b8ddf5780
tophat8855 <hrfarley@gmail.com>
2014-12-22 11:53:31 -0800

    user registration test edited

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true, confirmation: true
+  validates :user_name, presence: true, uniqueness: true
   has_secure_password
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 feature "Users" do
-  scenario "User creates a user" do
-    visit users_path
-    click_on "New User"
-    fill_in "User Name", with: "User1"
+  scenario "User registers" do
+    visit signup_path
+    fill_in "User name", with: "User1"
     fill_in "Email", with: "fake@email.com"
     fill_in "Password", with: "123"
     fill_in "Password confirmation", with: "123"
-    click_on "Register"
+    click_on "Sign up"
 
     expect(page).to have_content ("Welcome, #{User1}")
   end
