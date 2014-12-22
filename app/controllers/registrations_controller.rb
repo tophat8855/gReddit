fb38b86568c8590dc8dd718e1b3af95d30c534c7
tophat8855 <hrfarley@gmail.com>
2014-12-22 11:49:35 -0800

    add registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,15 @@
 class RegistrationsController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:user_name, :email, :password, :password_confirmation))
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 root 'welcome#index'
 get '/sign-up' => 'registrations#new', as: :signup
+post '/sign-up' => 'registrations#create'
 
 resources :users
 
