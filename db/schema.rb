ecef11fe5a58a62719ee6802787713e5a85fbba2
tophat8855 <hrfarley@gmail.com>
2015-01-04 21:07:20 -0800

    create moderators with moderation abilities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141222222248) do
+ActiveRecord::Schema.define(version: 20150105042505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20141222222248) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "moderate"
   end
 
 end
